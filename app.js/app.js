cf6f8c62df5099cc4f66622b3f2ee13fb350ecaa
Nafortinera <106200801+Nafortinera@users.noreply.github.com>
2022-06-27 15:15:24 -0300

    nuevo

--- app.js/app.js
+++ app.js/app.js
@@ -7,8 +7,10 @@ const container = document.getElementById ("container")
 form.addEventListener ("submit", e=>{
     e.preventDefault ()
     let container = ""
+    let regexCorreo= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     if(nombre.value.length <6){
         container +=`El nombre no es valido` <br>
+    if (regexCorreo.test(email.value))
        
     }
 })
