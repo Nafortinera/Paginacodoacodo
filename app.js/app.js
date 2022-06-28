15140d5d17e73b5797136a15976e441456825521
Nafortinera <106200801+Nafortinera@users.noreply.github.com>
2022-06-28 16:09:11 -0300

    nuevo

--- app.js/app.js
+++ app.js/app.js
@@ -2,19 +2,31 @@ const nombres = document.getElementById ("nombres")
 const correo = document.getElementById ("correo")
 const comentarios = document.getElementById ("comentarios")
 const form = document.getElementById ("form")
-const row = document.getElementById ("row")
+const container = document.getElementById ("row")
 
 form.addEventListener ("submit", e=>{
     e.preventDefault ()
-    let row = ""
+    let container = ""
+    let enviar = false
     let regexCorreo= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     if(nombres.value.length <6){
-        row +=`El nombre no es valido` <br>
+        container +=`El nombre no es valido` <br>
+        enviar = true
     }
 
     console.log (regexCorreo.test(correo.value))
-    if (regexCorreo.test(correo.value)){
-       
+    if (!regexCorreo.test(correo.value)){
+        container +=`El correo no es valido` <br>
+        enviar = true
        
     }
+    if (comentarios.value.length<100){
+        enviar = true
+    
+    }
+    if (enviar){
+        parrafo.innerHTML = ""
+        container
+    }
+
 })
