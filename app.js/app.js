5f5600739c575e1e5a2fb9608bb7770e771ad84d
Nafortinera <106200801+Nafortinera@users.noreply.github.com>
2022-06-28 15:46:18 -0300

    nuevo

--- app.js/app.js
+++ app.js/app.js
@@ -2,17 +2,19 @@ const nombres = document.getElementById ("nombres")
 const correo = document.getElementById ("correo")
 const comentarios = document.getElementById ("comentarios")
 const form = document.getElementById ("form")
-const container = document.getElementById ("container")
+const row = document.getElementById ("row")
 
 form.addEventListener ("submit", e=>{
     e.preventDefault ()
-    let container = ""
+    let row = ""
     let regexCorreo= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    if(nombre.value.length <6){
-        container +=`El nombre no es valido` <br>
+    if(nombres.value.length <6){
+        row +=`El nombre no es valido` <br>
+    }
 
-    if (!regexCorreo.test(email.value))
-        container +=`El nombre no es valido` <br>
+    console.log (regexCorreo.test(correo.value))
+    if (regexCorreo.test(correo.value)){
+       
        
     }
 })
