e7324d541c010bfdaa4be6e4f90eabc0cc214287
Nafortinera <106200801+Nafortinera@users.noreply.github.com>
2022-06-28 20:18:03 -0300

    nuevo

--- app.js/app.js
+++ app.js/app.js
@@ -1,12 +1,12 @@
-const nombres = document.getElementById ("nombres")
-const correo = document.getElementById ("correo")
+const nombre = document.getElementById ("nombres")
+const email = document.getElementById ("correo")
 const comentarios = document.getElementById ("comentarios")
 const form = document.getElementById ("form")
-const container = document.getElementById ("container")
+const warnings = document.getElementById ("container")
 
 form.addEventListener ("submit", e=>{
     e.preventDefault ()
-    let container = ""
+    let warnings = ""
     let enviar = false
     let regexCorreo= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     if(nombres.value.length <6){
