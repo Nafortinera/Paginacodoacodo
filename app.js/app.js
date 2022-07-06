4db7c509d4f7b4ca5c1699ae03b79d8e485632fd
Nafortinera <106200801+Nafortinera@users.noreply.github.com>
2022-07-06 11:06:44 -0300

    nuevo

--- app.js/app.js
+++ app.js/app.js
@@ -4,29 +4,32 @@ const comentarios = document.getElementById ("comentarios")
 const form = document.getElementById ("form")
 const parrafo = document.getElementById ("warnings")
 
-form.addEventListener ("submit", e=>{
+form.addEventListener ("submit", e=> {
+
     e.preventDefault ()
     let warnings = ""
-    let entrar = false
     let regexEmail= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    parrafo.innerHTML ""
-    if(nombre.value.length <6){
-        warnings +=`El nombre no es valido <br>`
-        entrar = true
-    }
+    let entrar = false
+    parrafo.innerHTML = ""
 
-    
-    if (!regexEmail.test(email.value)){
-        warnings +=`El correo no es valido <br>`
-        entrar = true
-       
+    if(nombre.value.length <6){
+    alert("nombre muy corto")
+    warnings +=`El nombre no es valido <br>`
+    entrar = true
     }
+    console.log (!regexEmail.test(email.value))
+    if (!regexEmail.test(email.value))
+    entrar = true
+
     if (comentarios.value.length<50){
         entrar = true
-    
+
     }
     if (entrar){
         parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado"
+    
     }
 
 })
